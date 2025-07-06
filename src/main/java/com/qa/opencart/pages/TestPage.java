f211c4c739f96015d929b278bad2f10a36631a9c
Bikram2121 <vikram.mohanty40@gmail.com>
2025-07-06 19:14:01 +0530

    added a feature file

--- src/main/java/com/qa/opencart/pages/TestPage.java
+++
@@ -1,3 +0,0 @@
-public class TestPage {
-  int i =  20;
-}
---
+++ src/main/java/features/Feature.java
@@ -0,0 +1,7 @@
+package features;
+
+public class Feature {
+    public static void main(String[] args) {
+        System.out.println("This is a feature file");
+    }
+}
